1bc0d82106f16079049d0f390d07f7f1dfa1f60e
PedroCorcaque <pedrollcorc@gmail.com>
2023-02-09 14:34:14 -0300

    Get transformations of local structures of IFC4 model

--- ifc_analyzer.py
+++ ifc_analyzer.py
@@ -1,20 +1,19 @@
 import ifcopenshell
+import ifcopenshell.geom
 
-from libs import ifc4_analyzer, ifc2x3_analyzer
+from libs import ifc2x3_analyzer, ifc4_analyzer
 
-import inspect
-
-RIGHT_FILENAME = "/home/pedro/Dev/aeroscan_ws/data/ifc/Staircase.ifc"
-WRONG_FILENAME = "/home/pedro/Dev/aeroscan_ws/data/ifc/mapping_IFC4_Convenience_store.ifc"
-LIST_OF_ALL_FILES = [RIGHT_FILENAME, WRONG_FILENAME]
+ifc2x3_file = "/home/pedro/Dev/aeroscan_ws/data/ifc/Staircase.ifc"
+ifc4_file = "/home/pedro/Dev/aeroscan_ws/data/ifc/mapping_IFC4_Convenience_store.ifc"
+ALL_FILES = [ifc2x3_file, ifc4_file]
 
 if __name__ == "__main__":
 
     settings = ifcopenshell.geom.settings()
-    for file in LIST_OF_ALL_FILES:
+    for file in ALL_FILES:
         if ifcopenshell.open(file).schema == "IFC2X3":
             ifc2x3_analyzer(file, settings)
         elif ifcopenshell.open(file).schema == "IFC4":
             ifc4_analyzer(file, settings)
         else:
-            raise Exception("Schema is not found.")
+            raise Exception("Schema not found.")
